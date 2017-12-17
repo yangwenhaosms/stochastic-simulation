845d7f10504b30a5c7b1cd42204a103d984d9353
Wenhao Yang <yangwhsms@gmail.com>
2017-12-17 23:48:16 +0800

    update result

--- project6/result/plot.py
+++ project6/result/plot.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    x = np.loadtxt('./mc-Euler-Maruyama-strong.txt')
-    y = np.loadtxt('./mc-Milstein-strong.txt')
+    x = np.loadtxt('./mc-Euler-Maruyama-weak.txt')
+    y = np.loadtxt('./mc-Milstein-weak.txt')
     a = np.arange(1, 11, 1)
     plt.plot(a, x)
     plt.plot(a, y)
     label = ['Euler-Maruyama', 'Milstein']
     plt.legend(label, loc = 'upper right')
     fig = plt.gcf()
-    fig.savefig('./mc-strong.eps')
+    fig.savefig('./mc-weak.eps')
