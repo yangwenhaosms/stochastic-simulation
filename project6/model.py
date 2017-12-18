be6001f75b38327a3da7d6605f0b046117da7b26
yangwenhao <yangwhsms@gmail.com>
2017-12-19 02:08:38 +0800

    update result

--- project6/model.py
+++ project6/model.py
@@ -51,12 +51,12 @@ class Black_Scholes_Multilevel(object):
     def simulate(self):
         delta_w_com = 0
         if self.method == 'Euler-Maruyama':
-            for _ in range(self.n):
+            for i in range(self.n):
                 delta_w = np.random.normal(0.0, np.sqrt((self.t1 - self.t0)/self.n))
                 self.record_w += delta_w
                 delta_w_com += delta_w
-                if _ % 2 == 1:
-                    s_com = self.record_s_com[-1] + self.mu * self.record_s_com[-1] * 2 *(self.t1 - self.t0)/self.n \
+                if i % 2 == 1:
+                    s_com = self.record_s_com[-1] + self.mu * self.record_s_com[-1] * 2 * (self.t1 - self.t0)/self.n \
                             + self.sigma * self.record_s_com[-1] * delta_w_com
                     delta_w_com = 0
                     self.record_s_com.append(s_com)
@@ -83,4 +83,5 @@ class Black_Scholes_Multilevel(object):
         res = self.s0 * np.exp((self.mu - 0.5 * self.sigma**2) * (self.t1 - self.t0) + self.sigma * self.record_w)
         self.record_w = 0
         self.record_s = [self.s0]
+        self.record_s_com = [self.s0]
         return res_hat, res_hat_com, res
