7202c76b0a9d22392d3d8dd21844b4026205b421
Wenhao Yang <yangwhsms@gmail.com>
2017-12-24 02:52:21 +0800

    update result

--- project6/result/plot.py
+++ project6/result/plot.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    x = np.loadtxt('./mmc-var-Euler-Maruyama.txt')
-    y = np.loadtxt('./mmc-var-Milstein.txt')
-    a = np.arange(1, 11, 1)
-    plt.plot(a, x[1:])
-    plt.plot(a, y[1:])
+    x = np.loadtxt('./mmc-err-Euler-Maruyama.txt')
+    y = np.loadtxt('./mmc-err-Milstein.txt')
+    a = np.arange(1, 5, 1)
+    plt.plot(a, x)
+    plt.plot(a, y)
+    plt.xlabel('-log(Epsilon)')
+    plt.ylabel('Absolute mean error')
     label = ['Euler-Maruyama', 'Milstein']
     plt.legend(label, loc = 'upper right')
     fig = plt.gcf()
-    fig.savefig('./mmc-var.eps')
+    fig.savefig('./mmc-err.eps')
