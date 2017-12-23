c770c8d7d70a332e18ccf30f935c5b87fccaabd8
Wenhao Yang <yangwhsms@gmail.com>
2017-12-24 02:14:22 +0800

    Auto stash before merge of "master" and "origin/master"

--- project6/multimain.py
+++ project6/multimain.py
@@ -35,10 +35,11 @@ def mul_main(mu, sigma, s0, t0, t1, algorithm, url_worker, num_worker, k, epsilo
     # how to define delta in a more generalized form is undone
     delta = (t1 - t0) / (2**np.arange(0, 11, 1))
     num_sample = np.ceil(2 * np.sqrt(var * delta) * np.sum(np.sqrt(var/delta)) / (epsilon ** 2)).astype(np.int32)
-    res_hat = 0
+    res, _ = train_mc(mu, sigma, s0, t0, t1, algorithm, 0, url_worker, num_worker, num_sample[0])
+    res_hat = np.mean(res)
     for i in range(k):
-        res_l, res_l_1, _ = train_mmc(mu, sigma, s0, t0, t1, algorithm, i+1, url_worker, num_worker, num_sample[i])
-
+        res_l, res_l_1, _ = train_mmc(mu, sigma, s0, t0, t1, algorithm, i+1, url_worker, num_worker, num_sample[i+1])
+        res_hat += np.mean(res_l - res_l-1)
 
 
 
@@ -53,11 +54,15 @@ def main():
     parser.add_argument('--k', help='maximum level', type=int, default=10)
     parser.add_argument('--url_worker', help='the url of worker', type=str, default='ipc://homework')
     parser.add_argument('--num_worker', help='the number of workers', type=int, default=16)
-    parser.add_argument('--epsilon', help='the accuracy', type=float, default=0.01)
     args = parser.parse_args()
 
-    var = get_variance(args.mu, args.sigma, args.s0, args.t0, args.t1, args.algorithm, args.url_worker, args.num_worker, args.k)
-    np.savetxt('./result/mmc-var-{}.txt'.format(args.algorithm), np.array([var]), fmt='%f '*(1+args.k))
+    # var = get_variance(args.mu, args.sigma, args.s0, args.t0, args.t1, args.algorithm, args.url_worker, args.num_worker, args.k)
+    # np.savetxt('./result/mmc-var-{}.txt'.format(args.algorithm), np.array([var]), fmt='%f '*args.k)
+
+    epsilons = [0.1, 0.01, 0.001, 0.0001]
+    baseline = train_mc(args.mu, args.sigma, args.s0, args.t0, args.t1, args.algorithm, 0, args.url_worker, args.num_worker, int(1e6))
+    for epsilon in epsilons:
+        res = mul_main
 
 
 if __name__ == '__main__':
