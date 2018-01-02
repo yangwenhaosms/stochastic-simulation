9b3cac890d4836516041e09e95146efbb87910fc
Wenhao Yang <yangwhsms@gmail.com>
2018-01-03 01:35:44 +0800

    update result

--- project8/result/plot.py
+++ project8/result/plot.py
@@ -2,11 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    x = np.loadtxt('./GUE-N-10.txt')[:,-1]
+    x = np.loadtxt('./GUE-EIG-N-5000.txt')
     plt.hist(x, 500)
-    mean = np.mean(x)
-    var = np.var(x)
-    plt.xlabel('scaled spaceing')
-    plt.title(r'Histogram: $\mu={:.3f}$, $\sigma={:.3f}$'.format(mean, np.sqrt(var)))
     fig = plt.gcf()
-    fig.savefig('./GUE-N-10.eps')
+    fig.savefig('./GUE-EIG-N-5000.eps')
